f745eb724ac7d39998f9acfaed3cfe42ea6d9fe7
jstoveld <jstoveld@gmail.com>
2020-04-28 20:57:42 -0400

    Checking in days work as the project now can scrape form the webpage on command, added comments and doc strings as well as proper 2 line spaces per blocks / functions.

--- src/014/corepy/words.py
+++ src/014/corepy/words.py
@@ -1,9 +1,53 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+
+    python3 words.py <url>
+"""
+
+import sys
 from urllib.request import urlopen
+#eg. http://sixty-north.com/c/t.txt
 
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-    line_words = line.decode('utf8').split()
-    for word in line_words:
-        story_words.append(word)
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+    
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from the document.        
+    """
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
+    return story_words
+
+
+def print_items(items):
+    """Print items one per line.
+
+        Args:
+            An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print each word form a text document from a URL.
+
+        Args:
+            url: The URL of a UTF-8 text document.
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1]) #The 0th arg is the module of the filename.
