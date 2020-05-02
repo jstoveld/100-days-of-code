c54fa4cfae546eb1b0fd6dd881005f6e09e240e4
jstoveld <jstoveld@gmail.com>
2020-05-02 10:59:08 -0400

    Added the feature of requesting the stock desired from the users input. Then input as a variable is inserted as a literal string into the requested scraped site.

--- src/018/stockPriceUpdate.py
+++ src/018/stockPriceUpdate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created 4-30-20 4:29:00PM
+Created 5-2-20 10:14:00AM
 
 @author: JS
 """
@@ -14,7 +14,7 @@ stock = input("Please type in a stock ticker symbol to query:\n")
 #Definining our function parsePrice that will go and look for a specific element on the below URL
 #Returning the current price
 def prasePrice():
-    r=requests.get('https://finance.yahoo.com/quote/{stock}')
+    r=requests.get(f'https://finance.yahoo.com/quote/{stock}')
     soup=bs4.BeautifulSoup(r.text, "html.parser")
     price=soup.find_all('div',{'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
     return price
