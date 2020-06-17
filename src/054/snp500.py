1286ead05dfb66a7521f5dbfa63f904eec26da36
jstoveld <jstoveld@gmail.com>
2020-06-17 18:24:40 -0400

    Correlated data but now we have a big green blob to finesse!

--- src/054/snp500.py
+++ src/054/snp500.py
@@ -7,6 +7,10 @@ Edited Last: 6-15-20 05:20 PM
 
 
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 import pickle
 import requests
 
@@ -27,6 +31,29 @@ def save_sp500_tickers():
 
     print(tickers)
     return tickers
+save_sp500_tickers
 
-    
-save_sp500_tickers()
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open ("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2020,1,1)
+    end = dt.datetime(2020,6,16)
+
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format((ticker)))
+
+save_sp500_tickers()            
+get_data_from_yahoo()
+
